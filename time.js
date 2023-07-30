32a2b97e81f63ca050dfeb1bd53014b66dc50d46
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-07-30 18:50:24 +0600

    Add files via upload

---
+++ time.js
@@ -0,0 +1,18 @@
+const  route44 = document.getElementById("44");
+const  route115 = document.getElementById("115");
+const  route159 = document.getElementById("159");
+const image = document.getElementById("timetable");
+
+route44.addEventListener("click", function() {
+    image.src = "images/route 44.png";
+
+});
+
+route115.addEventListener("click", function() {
+    image.src = "images/route 115.png";
+});
+
+route159.addEventListener("click", function() {
+    image.src = "images/route 159.png";
+
+});
