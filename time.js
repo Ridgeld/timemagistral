f5d0345922806ce873522ea0bb50e2fd40892950
Ridgeld <124894477+Ridgeld@users.noreply.github.com>
2023-07-30 19:38:57 +0600

    Update time.js

--- time.js
+++ time.js
@@ -1,13 +1,13 @@
 const  route44 = document.getElementById("44");
 const  route115 = document.getElementById("115");
 const  route159 = document.getElementById("159");
-const image = document.getElementById("timetable");
 const text = document.getElementById("routeName");
 
 route44.addEventListener("click", function() {
     text.textContent = "Маршрут №44";
 });
 route115.addEventListener("click", function() {
+    var image = document.getElementById("timetable");
     image.src = "images/time115.png";
     text.textContent = "Маршрут №115";
 });
